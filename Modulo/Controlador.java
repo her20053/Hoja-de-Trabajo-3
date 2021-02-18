e192113a935241148b758ef87cd0ba53d4e6a974
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2021-02-18 09:38:31 -0600

    Implementado primer algoritmo

---
+++ Modulo/Controlador.java
@@ -0,0 +1,239 @@
+package Modulo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Controlador {
+
+  private static int[] listaDesordenada;
+  
+  public static void main(String[] args) {
+      
+      vista();
+      generarArchivoNumeros();
+      GnomeSort(listaDesordenada, listaDesordenada.length);
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  private static void vista(){
+
+      System.out.println("\n" + "-".repeat(80));
+      System.out.println(" > ] Bienvenido, un archivo sera generado con numeros desde 0 < x < 3001.");
+      System.out.println("     El programa creara archivos por cada metodo de Sort posteriormente..\n");
+      System.out.println(" 1 ] Gnome  sorting algorithm.");
+      System.out.println(" 2 ] Merge  sorting algorithm.");
+      System.out.println(" 3 ] Quick  sorting algorithm.");
+      System.out.println(" 4 ] Radix  sorting algorithm.");
+      System.out.println(" 5 ] Buble  sorting algorithm.\n");
+
+      var sc = new Scanner(System.in);
+      System.out.print(" > ] Presione ENTER para comenzar el recorrido.. ");
+      sc.nextLine();
+      sc.close();
+
+
+  }
+
+  private static void generarArchivoNumeros(){
+
+      System.out.println("\n[Consola] Generando lista de numeros..");
+
+      File aEliminar = new File("numerosAleatorios.txt"); 
+      if (aEliminar.delete()) { 
+      System.out.println("[Consola] Archivo de datos anterior eliminado: " + aEliminar.getName());
+      } else {
+      System.out.println("[Consola] No se ha reconocido ningun archivo previo .txt, continuando.");
+      }
+      
+      try {
+      File aCrear = new File("numerosAleatorios.txt");
+          if (aCrear.createNewFile()) {
+            System.out.println("[Consola] Archivo creado: " + aCrear.getName());
+          }
+        } catch (IOException e) {
+          System.out.println("[Consola] Error al crear archivo.");
+          e.printStackTrace();
+      }
+      try {
+          FileWriter myWriter = new FileWriter("numerosAleatorios.txt");
+          ArrayList<Integer> lista = new ArrayList<>();
+          for(int i = 1; i < 3001; i++){
+              lista.add(i);
+          }
+          Collections.shuffle(lista);
+          listaDesordenada = new int[lista.size()];
+          for(int i = 0; i < lista.size(); i++){
+            listaDesordenada[i] = lista.get(i);
+          }
+          String objetoTString = lista.stream().map(Object::toString).collect(Collectors.joining(" "));
+          myWriter.write(objetoTString);
+          myWriter.close();
+          System.out.println("[Consola] Se han escrito los datos correctamente al archivo.");
+        } catch (IOException e) {
+          System.out.println("[Consola] Ocurrio un error.");
+          e.printStackTrace();
+        }
+  }
+  private static void GnomeSort(int estructura[], int largo){ 
+    int puntero = 0; 
+    while (puntero < largo) { 
+        if (puntero == 0) 
+            puntero++; 
+        if (estructura[puntero] >= estructura[puntero - 1]) 
+            puntero++; 
+        else { 
+            int temporal = 0; 
+            temporal = estructura[puntero]; 
+            estructura[puntero] = estructura[puntero - 1]; 
+            estructura[puntero - 1] = temporal; 
+            puntero--; 
+        } 
+    } 
+    System.out.println(Arrays.toString(estructura));
+} 
+
+}
